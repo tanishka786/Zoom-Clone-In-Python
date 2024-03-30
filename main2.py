18fc0c02ca8f9fe6f8213af929800eebeb025d65
tan-demo <tanishkajdas@gmail.com>
2024-03-30 19:48:40 +0530

    made few changes

--- main2.py
+++ main2.py
@@ -12,8 +12,8 @@ local_ip_address = socket.gethostbyname(socket.gethostname())
 
 ########################################## Adding functionalities to buttons created ########################################################
 
-server = StreamingServer(local_ip_address, 9999)
-receiver = AudioReceiver(local_ip_address, 8888)
+server = StreamingServer(local_ip_address, 7777)
+receiver = AudioReceiver(local_ip_address, 6666)
 
 #Note: we are not calling the functions, we are just refering them so eg, start_server() is wriitrn without this '()'
 
@@ -27,19 +27,19 @@ def start_listening():
 
 #adding function for video
 def start_camera_stream():
-    camera_client = CameraClient(text_target_ip.get(1.0,'end-1c'), 7777)
+    camera_client = CameraClient(text_target_ip.get(1.0,'end-1c'), 9999)
     t3 = threading.Thread(target = camera_client.start_stream)
     t3.start()
 
 #adding function for screen sharing
 def start_screen_sharing():
-    screen_client = ScreenShareClient(text_target_ip.get(1.0,'end-1c'), 7777)
+    screen_client = ScreenShareClient(text_target_ip.get(1.0,'end-1c'), 9999)
     t4 = threading.Thread(target = screen_client.start_stream)
     t4.start()
 
 #adding function for audio streaming
 def start_audio_stream():
-    audio_sender = AudioSender(text_target_ip.get(1.0,'end-1c'), 6666)
+    audio_sender = AudioSender(text_target_ip.get(1.0,'end-1c'), 8888)
     t5 = threading.Thread(target = audio_sender.start_stream)
     t5.start()
 
