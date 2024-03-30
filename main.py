45236e92dfa75e914fe139fbbac49437a99a67df
tan-demo <tanishkajdas@gmail.com>
2024-03-31 01:12:00 +0530

    made few changes in Entry box

--- main.py
+++ main.py
@@ -32,19 +32,19 @@ def start_listening():
 
 #adding function for video
 def start_camera_stream():
-    camera_client = CameraClient(text_target_ip.get(1.0,'end-1c'), 7777)
+    camera_client = CameraClient(entry1.get(1.0,'end-1c'), 7777)
     t3 = threading.Thread(target = camera_client.start_stream)
     t3.start()
 
 #adding function for screen sharing
 def start_screen_sharing():
-    screen_client = ScreenShareClient(text_target_ip.get(1.0,'end-1c'), 7777)
+    screen_client = ScreenShareClient(entry1.get(1.0,'end-1c'), 7777)
     t4 = threading.Thread(target = screen_client.start_stream)
     t4.start()
 
 #adding function for audio streaming
 def start_audio_stream():
-    audio_sender = AudioSender(text_target_ip.get(1.0,'end-1c'), 6666)
+    audio_sender = AudioSender(entry1.get(1.0,'end-1c'), 6666)
     t5 = threading.Thread(target = audio_sender.start_stream)
     t5.start()
 
