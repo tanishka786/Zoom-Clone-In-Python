11f6f35b9cf7f80d5e67d085b33324812f9b3f52
tan-demo <tanishkajdas@gmail.com>
2024-03-30 19:31:58 +0530

    Added few functionalities

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ text_target_ip = tk.Text(window, height=1)
 text_target_ip.pack()
 
 #creating a button which says: start listing to incoming connections
-btn_listen = tk.Button(window, text="Start Listening", width=50)
+btn_listen = tk.Button(window, text="Start Listening", width=50 )
 btn_listen.pack(anchor=tk.CENTER, expand=True)
 
 #Creating a button for camera
@@ -42,4 +42,38 @@ btn_audio = tk.Button(window, text="Start Audio Stream", width=50)
 btn_audio.pack(anchor=tk.CENTER, expand=True)
 
 window.mainloop()
+
+########################################## Adding functionalities to buttons created ########################################################
+
+server = StreamingServer(local_ip_address, 9999)
+receiver = AudioReceiver(local_ip_address, 8888)
+
+#Note: we are not calling the functions, we are just refering them so eg, start_server() is wriitrn without this '()'
+
+#adding function for connecting two servers
+def start_listening():
+    t1 = threading.Thread(target = server.start_server)
+    t2 = threading.Thread(target = receiver.start_server)
+    t1.start()
+    t2.start()
+
+
+#adding function for video
+def start_camera_stream():
+    camera_client = CameraClient(text_target_ip.get(1.0,'end-1c'), 7777)
+    t3 = threading.Thread(target = camera_client.start_stream)
+    t3.start()
+
+#adding function for screen sharing
+def start_screen_sharing():
+    screen_client = ScreenShareClient(text_target_ip.get(1.0,'end-1c'), 7777)
+    t4 = threading.Thread(target = screen_client.start_stream)
+    t4.start()
+
+#adding function for audio streaming
+def start_audio_stream():
+    audio_sender = AudioSender(text_target_ip.get(1.0,'end-1c'), 6666)
+    t5 = threading.Thread(target = audio_sender.start_stream)
+    t5.start()
+
 
