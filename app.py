9a96c9bbf21d439b2843d88bc76bc82723f91ae7
tan-demo <tanishkajdas@gmail.com>
2024-04-17 00:15:40 +0530

    made few changes

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from vidstream import *
 import tkinter as tk
 import socket
 import threading
-import requests # type: ignore
+import requests
 
 local_ip_addr = socket.gethostbyname(socket.gethostname())
 public_ip_addr = requests.get("https://api.ipify.org").text
